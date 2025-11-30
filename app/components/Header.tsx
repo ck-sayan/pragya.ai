b868909b6129f0c2ad4e13b2b0729593b7c3e4a8
Sayan Chakraborty <105535261+ck-sayan@users.noreply.github.com>
2025-11-30 12:57:18 +0530

    changed the header for better outlook

--- app/components/Header.tsx
+++ app/components/Header.tsx
@@ -2,7 +2,7 @@ export default function Header() {
   return (
     <header className="header">
       <div className="header__title">
-        <h1>Hello, There!</h1>
+        <h1>Heyy, There!</h1>
         <h2>How are you feeling today?</h2>
       </div>
     </header>
